778d2fe57bb9fa5bdf20e001f099abebdc091cc9
C. Thomas Brittain <thomasb@bitfocus.com>
2024-01-20 07:34:42 -0600

    Setup a debugging visual for VoxelGrid

--- src/main.rs
+++ src/main.rs
@@ -27,25 +27,16 @@ fn main() {
                     position: WindowPosition::At(IVec2::new(1720, 0)),
                     resolution: RESOLUTION.into(),
                     present_mode: PresentMode::AutoVsync,
-                    // Tells wasm to resize the window according to the available canvas
-                    // fit_canvas_to_parent: true,
-                    // // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
-                    // prevent_default_event_handling: false,
-                    // window_theme: Some(WindowTheme::Dark),
                     enabled_buttons: bevy::window::EnabledButtons {
                         maximize: false,
                         ..Default::default()
                     },
-                    // This will spawn an invisible window
-                    // The window will be made visible in the make_visible() system after 3 frames.
-                    // This is useful when you want to avoid the white window that shows up before the GPU is ready to render the app.
-                    // visible: true,
                     ..Default::default()
                 }),
                 ..Default::default()
             }),
             // WorldInspectorPlugin::new(),
-            // WireframePlugin::default(),
+            WireframePlugin::default(),
             LookTransformPlugin,
             FpsCameraPlugin::default(),
         ))
@@ -85,7 +76,7 @@ pub const GAME_BOUNDS: Bounds2D = Bounds2D {
 };
 
 pub const ON_COLOR: Color = Color::rgba(0.8, 0.1, 0.0, 1.0);
-pub const OFF_COLOR: Color = Color::rgba(0.0, 0.1, 0.8, 1.0);
+pub const OFF_COLOR: Color = Color::rgba(0.33, 0.9, 0.33, 1.0);
 
 pub fn setup(
     mut commands: Commands,
@@ -93,76 +84,13 @@ pub fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let resolution = 4 as usize;
-    let voxel_grid = VoxelGrid::new(&[[0.0, 0.0, 0.0]], resolution);
-    voxel_grid.debug(&mut |x, y, z, resolution, noise| {
-        commands.spawn(PbrBundle {
-            mesh: meshes.add(shape::Cube { size: 0.10 }.try_into().unwrap()),
-            material: materials.add(StandardMaterial {
-                base_color: OFF_COLOR,
-                emissive: OFF_COLOR,
-                alpha_mode: AlphaMode::Blend,
-                ..Default::default()
-            }),
-            transform: Transform::from_xyz(x, y, z),
-            ..Default::default()
-        });
-    });
 
-    // voxel_grid.
-
-    // let mc = MarchingCubes::new(&Mesh::from(shape::Cube::new(0.40)), resolution);
-    // mc.march_all();
-    // for position in mc.voxel_grid.read_all().chunks_exact(3) {
-    //     // Create a small isosphere at each point
-
-    //     commands.spawn(PbrBundle {
-    //         mesh: meshes.add(
-    //             shape::Icosphere {
-    //                 radius: 0.010,
-    //                 subdivisions: 4,
-    //             }
-    //             .try_into()
-    //             .unwrap(),
-    //         ),
-    //         material: materials.add(StandardMaterial {
-    //             base_color: Color::rgba(0.8, 0.1, 0.0, 1.0),
-    //             emissive: Color::rgba(0.8, 0.1, 0.0, 01.0),
-    //             alpha_mode: AlphaMode::Blend,
-    //             ..Default::default()
-    //         }),
-    //         transform: Transform::from_xyz(position[0], position[1], position[2]),
-    //         ..Default::default()
-    //     });
-    // }
-
-    // let mut geo_node = GeoNode::from_mesh(Mesh::from(shape::Cube::new(0.40)));
-    // let mut geo_node2 = GeoNode::from_mesh(Mesh::from(shape::Cube::new(0.80)));
-
-    // // Set scale
-    // const CUBE_SCALE: Vec3 = Vec3::new(10.0, 10.0, 10.0);
-    // geo_node.scale = CUBE_SCALE;
-    // // geo_node2.scale = CUBE_SCALE - 4.0;
-
-    // geo_node.combine(geo_node2);
-
-    // // Add color to the mesh
-    // geo_node.material = StandardMaterial {
-    //     base_color: Color::rgba(0.8, 0.1, 0.6, 0.02),
-    //     emissive: Color::rgba(0.8, 0.1, 0.6, 0.02),
-    //     alpha_mode: AlphaMode::Blend,
-    //     ..Default::default()
-    // };
-
-    // let mut pbr = geo_node.get_pbr_bundle(&mut meshes, &mut materials);
-
-    // pbr.transform.scale = CUBE_SCALE;
-
-    // commands.spawn((Camera3dBundle {
-    //     transform: Transform::from_xyz(0.0, 5.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
-    //     ..default()
-    // },));
-    let eye = Vec3::new(-2.0, 5.0, 5.0);
-    let target = Vec3::new(0., 0., 0.);
+    let eye = Vec3::new(-5.0, 5.0, -5.0);
+    let target = Vec3::new(
+        resolution as f32 / 3.0,
+        resolution as f32 / 3.0,
+        resolution as f32 / 3.0,
+    );
     let up = Vec3::Y;
 
     commands
@@ -173,16 +101,6 @@ pub fn setup(
             target,
             up,
         ));
-
-    // commands
-    //     .spawn(LookTransformBundle {
-    //         transform: LookTransform::new(eye, target, up),
-    //         smoother: Smoother::new(0.9), // Value between 0.0 and 1.0, higher is smoother.
-    //     })
-    //     .insert(Camera3dBundle {
-    //         transform: Transform::from_xyz(0.0, 5.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
-    //         ..default()
-    //     });
 
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
@@ -198,72 +116,23 @@ pub fn setup(
         ..default()
     });
 
+    let voxel_grid = VoxelGrid::new(&[[0.0, 0.0, 0.0]], resolution);
+    voxel_grid.debug(&mut |x, y, z, resolution, noise| {
+        commands.spawn(PbrBundle {
+            mesh: meshes.add(shape::Cube { size: 0.10 }.try_into().unwrap()),
+            material: materials.add(StandardMaterial {
+                base_color: OFF_COLOR,
+                emissive: OFF_COLOR,
+                alpha_mode: AlphaMode::Blend,
+                ..Default::default()
+            }),
+            transform: Transform::from_xyz(x, y, z),
+            ..Default::default()
+        });
+    });
+
     // commands
     //     .spawn(pbr)
     //     .insert(Name::new("Model"))
     //     .insert(Wireframe);
 }
-
-// pub fn camera_controls(
-//     keyboard: Res<Input<KeyCode>>,
-//     // mut game: ResMut<Game>,
-//     mut camera_query: Query<&mut Transform, With<Camera3d>>,
-//     mut entities: Query<&mut Transform, Without<Camera3d>>,
-//     time: Res<Time>,
-// ) {
-//     let mut camera = camera_query.single_mut();
-
-//     let mut forward = camera.forward();
-//     // forward.y = 0.0;
-//     forward = forward.normalize();
-//     // println!("{:?}", forward);
-
-//     let mut left = camera.left();
-//     left.y = 0.0;
-//     left = left.normalize();
-
-//     let mut down = camera.down();
-//     down.y = 0.0;
-//     down = down.normalize();
-
-//     let speed = CAMERA_MOVEMENT_SPEED;
-//     let rotate_speed = CAMERA_ROTATION_SPEED;
-
-//     if keyboard.pressed(KeyCode::W) {
-//         camera.translation += forward * time.delta_seconds() * speed;
-//     }
-//     if keyboard.pressed(KeyCode::S) {
-//         camera.translation -= forward * time.delta_seconds() * speed;
-//     }
-//     if keyboard.pressed(KeyCode::A) {
-//         camera.translation += left * time.delta_seconds() * speed;
-//     }
-//     if keyboard.pressed(KeyCode::D) {
-//         camera.translation -= left * time.delta_seconds() * speed;
-//     }
-//     if keyboard.pressed(KeyCode::E) {
-//         camera.rotate_axis(Vec3::Y, rotate_speed * time.delta_seconds())
-//     }
-//     if keyboard.pressed(KeyCode::Q) {
-//         camera.rotate_axis(Vec3::Y, -rotate_speed * time.delta_seconds())
-//     }
-//     if keyboard.pressed(KeyCode::Z) {
-//         camera.translation += down * time.delta_seconds() * speed;
-//     }
-//     if keyboard.pressed(KeyCode::X) {
-//         camera.translation -= down * time.delta_seconds() * speed;
-//     }
-
-//     // Zoom in/out
-//     // if keyboard.pressed(KeyCode::Z) {
-//     //     // Iterate through all meshes and zoom them in/out
-//     //     for mut transform in entities.iter_mut() {
-//     //         transform.scale *= 1.0 + time.delta_seconds();
-//     //     }
-//     // }
-//     // if keyboard.pressed(KeyCode::X) {
-//     //     for mut transform in entities.iter_mut() {
-//     //         transform.scale *= 1.0 - time.delta_seconds();
-//     //     }
-//     // }
-// }
